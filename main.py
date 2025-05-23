b927ccc1b3494f57420e832ab9f93a150bde9722
jackh725 <jackhaoinau@gmail.com>
2025-05-23 18:20:34 +1000

    Upadte3

--- main.py
+++ main.py
@@ -13,9 +13,13 @@ from machine import UART
 uart = UART(1,baudrate=9600, tx = Pin(4), rx = Pin(5))
 
 LIGHT_THRESHOLD = 20000
+LIGHT_CHECK_INTERVAL = 10000 #ms
 ALERT_THRESHOLD = 10  #cm if <10cm beep 3 times
 DIST_CHECK_INTERVAL = 1000 # ms
-DIST_DURATION = 5   #s
+DIST_DURATION = 2000   #ms
+DHT_CHECK_INTERVAL = 20000  #ms
+
+
 
 last_light_check = utime.ticks_ms()
 last_dist_check = utime.ticks_ms()
@@ -26,6 +30,13 @@ close_duration = 0
 
 buzzer_enabled = True
 prev_btn_state = 0
+
+
+#default value 
+temp = -1
+hum = -1
+light = -1
+distance = -1
 
 def send_uart_data(temp, hum,light, distance, alert_type):
     data = {
@@ -38,8 +49,10 @@ def send_uart_data(temp, hum,light, distance, alert_type):
     uart.write(ujson.dumps(data) +"\n")
 
 while True:
-    #btn test is ok, now turn on and off
     
+    now = utime.ticks_ms()
+    
+    #button press for buzzer toggle
     current_state = is_pressed()
     if current_state == 1 and prev_btn_state ==0:
         
@@ -47,59 +60,70 @@ while True:
         print("Button State is:", "Enable" if buzzer_enabled else "Disabled")
         beep(1)  #to nofity user the btn is pressed
         utime.sleep(0.5)
+    pre_btn_state = current_state
     
+    #check dht11 every 20 s
+    if utime.ticks_diff(now, last_dht_check)>= DHT_CHECK_INTERVAL:
+        temp, hum = read_dht11()
+        print("Temperature is:",temp, " Humidity is: ",hum)
+        send_uart_data(temp,hum, light, distance, None)
+        print("DHT11 data sent!")
+        last_dht_check = now
+        
     #light check every 10 s
-    if utime.ticks_diff(utime.ticks_ms(), last_light_check)>= 10000:
+    if utime.ticks_diff(now, last_light_check)>= LIGHT_CHECK_INTERVAL:
         light = read_light()
         print("Light Level: ", light)
         if light < LIGHT_THRESHOLD:
             print("Low Light Alert!")
-            beep(1)
-        last_light_check = utime.ticks_ms() 
+            if buzzer_enabled:
+                beep(1)
+            send_uart_data(temp, hum, light, distance, "low_light")
+            print("Light data sent!")
+        last_light_check = now
     
-    #check humidity every 20 s
-#     if utime.ticks_diff(utime.ticks_ms(), last_dht_check)>= 20000:
-#         temp, hum = read_dht11()
-#         print("Temperature is:",temp, " Humidity is: ",hum)
-#         last_dht_check = utime.ticks_ms()
+    #check distance every 1 s
+    if utime.ticks_diff(now, last_dist_check) >= DIST_CHECK_INTERVAL:
+        distance = get_distance()
+        print("Distance is : ", distance, "cm")
+        last_dist_check = now
     
-    #check distance every 0.5 s
-    if utime.ticks_diff(utime.ticks_ms(), last_dist_check) >= DIST_CHECK_INTERVAL:
-        dist = get_distance()
-        print("Distance is : ", dist, "cm")
-        last_dist_check = utime.ticks_ms()
-    
-        if 0 < dist < ALERT_THRESHOLD:
+        if 0 < distance < ALERT_THRESHOLD:
             below_alert_duration += DIST_CHECK_INTERVAL
         else:
             below_alert_duration = 0
             
         if below_alert_duration >= DIST_DURATION:
+            print("Too close! Alert Triggered!")
             if buzzer_enabled:
-                print("Too close!")
                 beep(3)    
-                
+             
             else:
-                print("Less than 10 cm but Buzzer disabled")
+                print("Too close! Less than 10 cm but Buzzer disabled by button")
+            send_uart_data(temp,hum,light, distance, "too_close")  
             below_alert_duration = 0
         
-    if utime.ticks_diff(utime.ticks_ms(), last_dht_check)>= 2000:    #change to 20000 = 20s when submit, now is 2000 is quick for test uart
-        temp, hum = read_dht11()
-        light = read_light()
-        distance = get_distance()
-         
-        alert_type = None
-         
-        if light< LIGHT_THRESHOLD:
-            alert_type = "light"
-        elif 0 < distance < ALERT_THRESHOLD:
-            alert_type = "too_close"
         
-        send_uart_data(temp, hum, light, distance, alert_type)
-        last_dht_check = utime.ticks_ms()
-         
+        
+    #below code is to test uart can send all the sensor data correctly
+#     if utime.ticks_diff(utime.ticks_ms(), last_dht_check)>= 2000:
+#         temp, hum = read_dht11()
+#         light = read_light()
+#         distance = get_distance()
+#          
+#         alert_type = None
+#          
+#         if light< LIGHT_THRESHOLD:
+#             alert_type = "light"
+#         elif 0 < distance < ALERT_THRESHOLD:
+#             alert_type = "too_close"
+#         
+#         send_uart_data(temp, hum, light, distance, alert_type)
+#         last_dht_check = utime.ticks_ms()
+
         
 utime.sleep(2)
     
+
 
 
