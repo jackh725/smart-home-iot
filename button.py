89e05202bd803120072612d4df564e0aec62d8e0
jackh725 <jackhaoinau@gmail.com>
2025-05-26 16:18:29 +1000

    update btn.py

--- button.py
+++ button.py
@@ -1,6 +1,10 @@
 from machine import Pin
 
-button = Pin(18, Pin.IN, Pin.PULL_DOWN)
+button = Pin(18, Pin.IN,Pin.PULL_DOWN)
+
+# while True:
+#     print(button.value())
+#     time.sleep(0.2)
 
 def is_pressed():
     return button.value() == 1
